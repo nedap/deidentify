7531baf2d9f09240da957d5468edc1866ac0b9ee
Jan Trienes <jan.trienes@nedap.com>
2020-06-02 16:09:37 +0200

    Add wrapper script to start cpu/gpu benchmarks

---
+++ scripts/benchmark.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=0
+python -m scripts.benchmark benchmark_gpu
+
+export CUDA_VISIBLE_DEVICES=""
+export MKL_NUM_THREADS=32
+python -m scripts.benchmark benchmark_cpu_32_threads
+
+export CUDA_VISIBLE_DEVICES=""
+export MKL_NUM_THREADS=16
+python -m scripts.benchmark benchmark_cpu_16_threads
+
+export CUDA_VISIBLE_DEVICES=""
+export MKL_NUM_THREADS=8
+python -m scripts.benchmark benchmark_cpu_8_threads
