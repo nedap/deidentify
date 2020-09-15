085f8dba1090c95ca4bc8e9980d5e241cc4210a7
Jan Trienes <jantrienes@users.noreply.github.com>
2020-09-15 13:01:17 +0200

    Handle platform-specific issue with strftime/strptime (#35)
    * Add try/catch around shift_date to account for platform specific case
    
    * Improve formatting

--- deidentify/surrogates/generators/date.py
+++ deidentify/surrogates/generators/date.py
@@ -150,7 +150,7 @@ def minimum_season_offsets(dates):
 class DateSurrogates(SurrogateGenerator):
 
     def __init__(self, annotations, year_shift_base=65, year_shift_fuzz=20, random_data=None):
-        super(DateSurrogates, self).__init__(annotations=annotations, random_data=random_data)
+        super().__init__(annotations=annotations, random_data=random_data)
 
         self.dates = self._parse_dates(self.annotations)
         self.year_shift = year_shift_base + self.random_data.randint(
@@ -191,14 +191,20 @@ class DateSurrogates(SurrogateGenerator):
             most_recent = self.dates[0]
 
         for date in self.dates:
-            # TODO: Add an annotation object that encapsules automatic replacement errors
             if isinstance(date, NullDate):
+                # TODO: Add an annotation object that encapsules automatic replacement errors
                 replaced.append(None)
                 continue
 
             adjusted = adjust_long_date_span(date, most_recent=most_recent, max_delta=89)
             delta = relativedelta(days=self.day_shift, years=self.year_shift)
-            shifted_date = shift_date(adjusted, delta)
-            replaced.append(shifted_date.date_string)
+
+            try:
+                shifted_date = shift_date(adjusted, delta)
+                replaced.append(shifted_date.date_string)
+            except ValueError:
+                # shift_date may fail in platform-specific implementations of strftime and strptime
+                # when they are not symmetric. See: https://bugs.python.org/issue32195
+                replaced.append(None)
 
         return replaced
