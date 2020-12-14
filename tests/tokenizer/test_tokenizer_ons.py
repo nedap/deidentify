5a5c1ac64e1fc03cb59b5132761860d342788017
Jan Trienes <jan.trienes@nedap.com>
2020-12-14 08:01:27 +0100

    Fix tests to accomodate change in spaCy sentence tokenizer

--- tests/tokenizer/test_tokenizer_ons.py
+++ tests/tokenizer/test_tokenizer_ons.py
@@ -2,30 +2,33 @@ from deidentify.tokenizer.tokenizer_ons import TokenizerOns
 
 tokenizer = TokenizerOns()
 
+
 def test_tokenizer():
     text = '=== Answer: 1234 ===\ntest a b c d.\n=== Report: 1234 ===\nMw. test test test'
     doc = tokenizer.parse_text(text)
 
     tokens = [t.text for t in doc]
 
-    assert tokens == ['=== Answer: 1234 ===\n', 'test', 'a', 'b', 'c',
-                      'd.', '\n', '=== Report: 1234 ===\n', 'Mw.', 'test', 'test', 'test']
+    assert tokens == [
+        '=== Answer: 1234 ===\n', 'test', 'a', 'b', 'c', 'd.', '\n', '=== Report: 1234 ===\n',
+        'Mw.', 'test', 'test', 'test'
+    ]
 
 
 def test_sentence_segmentation():
-    text = '=== Answer: 1234 ===\ntest a b c d.\n=== Report: 1234 ===\nMw. test test test'
+    text = '=== Answer: 1234 ===\nDit is een zin.\n=== Report: 1234 ===\nMw. heeft goed gegeten.'
     doc = tokenizer.parse_text(text)
     sents = [sent.text for sent in doc.sents]
 
     assert sents == [
         '=== Answer: 1234 ===\n',
-        'test a b c d.\n',
+        'Dit is een zin.\n',
         '=== Report: 1234 ===\n',
-        'Mw. test test test'
+        'Mw. heeft goed gegeten.'
     ]
 
     sents = list(doc.sents)
     assert [token.text for token in sents[0]] == ['=== Answer: 1234 ===\n']
-    assert [token.text for token in sents[1]] == ['test', 'a', 'b', 'c', 'd.', '\n']
+    assert [token.text for token in sents[1]] == ['Dit', 'is', 'een', 'zin', '.', '\n']
     assert [token.text for token in sents[2]] == ['=== Report: 1234 ===\n']
-    assert [token.text for token in sents[3]] == ['Mw.', 'test', 'test', 'test']
+    assert [token.text for token in sents[3]] == ['Mw.', 'heeft', 'goed', 'gegeten', '.']
