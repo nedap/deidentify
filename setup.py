f5e1b5cbc8671e1c93e4ff1c5bf74a371e349563
Jan Trienes <jan.trienes@uni-due.de>
2022-02-12 09:25:26 +0100

    Remove py36 test job

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
     ],
